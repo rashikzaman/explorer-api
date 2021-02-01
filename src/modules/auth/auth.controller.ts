4456a6f4ca28c918832156048d091015a2997f52
rashikIcarus <36659537+rashikIcarus@users.noreply.github.com>
2021-02-01 20:00:08 +0600

    Added jwt authentication with static user

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,8 @@
+import { Get } from '@nestjs/common';
 import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -10,6 +12,12 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,10 +4,22 @@ import { AuthController } from './auth.controller';
 import { UsersService } from '../users/users.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
-  imports: [UsersModule],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}

2a6573828e05c20edaddd00d14ab2089f0173e55
rashikIcarus <36659537+rashikIcarus@users.noreply.github.com>
2021-02-12 17:33:53 +0600

    Modified seeders

--- src/modules/resources/seeders/resource-type.seed.ts
+++ src/modules/resources/seeders/resource-type.seed.ts
@@ -4,10 +4,12 @@ import { ResourceType } from '../models/entities/resource-type.entity';
 
 export default class CreateResourceType implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
-    await factory(ResourceType)({ type: 'movie' }).create();
-    await factory(ResourceType)({ type: 'music' }).create();
-    await factory(ResourceType)({ type: 'video' }).create();
-    await factory(ResourceType)({ type: 'article' }).create();
-    await factory(ResourceType)({ type: 'game' }).create();
+    try {
+      await factory(ResourceType)({ type: 'movie' }).create();
+      await factory(ResourceType)({ type: 'music' }).create();
+      await factory(ResourceType)({ type: 'video' }).create();
+      await factory(ResourceType)({ type: 'article' }).create();
+      await factory(ResourceType)({ type: 'game' }).create();
+    } catch (e) {}
   }
 }
--- src/modules/visibility/seeds/create-visibility.seed.ts
+++ src/modules/visibility/seeds/create-visibility.seed.ts
@@ -4,8 +4,10 @@ import { Visibility } from '../models/entity/visibility.entity';
 
 export default class CreateVisibility implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
-    await factory(Visibility)({ type: 'public' }).create();
-    await factory(Visibility)({ type: 'private' }).create();
-    await factory(Visibility)({ type: 'invite_only' }).create();
+    try {
+      await factory(Visibility)({ type: 'public' }).create();
+      await factory(Visibility)({ type: 'private' }).create();
+      await factory(Visibility)({ type: 'invite_only' }).create();
+    } catch (e) {}
   }
 }

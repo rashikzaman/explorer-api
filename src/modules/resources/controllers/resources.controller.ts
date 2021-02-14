2c364caba2c4c9757f96c54cf18251356c7f2601
rashikIcarus <36659537+rashikIcarus@users.noreply.github.com>
2021-02-15 01:12:48 +0600

    Fixed resoruce post validation error

--- src/modules/resources/controllers/resources.controller.ts
+++ src/modules/resources/controllers/resources.controller.ts
@@ -64,7 +64,7 @@ export class ResourcesController {
       },
     ),
   )
-  //@UsePipes(new JoiValidationPipe(createResourceSchema))
+  @UsePipes(new JoiValidationPipe(createResourceSchema))
   create(
     @Body() createResourceDto: CreateResourceDto,
     @UploadedFiles() files,

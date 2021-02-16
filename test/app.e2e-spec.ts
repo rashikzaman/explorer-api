a88783afeafbed86573c9cef6fe920a3677fa993
rashikIcarus <36659537+rashikIcarus@users.noreply.github.com>
2021-02-17 01:25:41 +0600

    Updated e2e test for user resource

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -34,19 +34,19 @@ describe('App Api', () => {
 
   /** Authentication begins */
 
-  it(`if email and password is not present, it should return 401`, () => {
+  it(`Auth: if email and password is not present, it should return 401`, () => {
     return request(app.getHttpServer()).post('/auth/login').expect(401);
   });
 
-  it(`if user tries to register without proper post body, it should return 400`, () => {
+  it(`Auth: if user tries to register without proper post body, it should return 400`, () => {
     return request(app.getHttpServer()).post('/auth/register').expect(400);
   });
 
-  it(`if user access profile without valid access token, it should return 401`, () => {
+  it(`Auth: if user access profile without valid access token, it should return 401`, () => {
     return request(app.getHttpServer()).get('/profile').expect(401);
   });
 
-  it(`if email and password is present, it should return 200`, async () => {
+  it(`Auth: if email and password is present, it should return 200`, async () => {
     const res = await request(app.getHttpServer())
       .post('/auth/login')
       .send({ email: 'rashikzaman22@gmail.com', password: '123456' });
@@ -54,28 +54,28 @@ describe('App Api', () => {
     jwtToken = res.body.access_token; //saving jwtToken for future reference
   });
 
-  it(`if email or password incorrect, it should return 401`, async () => {
+  it(`Auth: if email or password incorrect, it should return 401`, async () => {
     const res = await request(app.getHttpServer())
       .post('/auth/login')
       .send({ email: 'rashikzaman22@gmail.com', password: '123456999' });
     expect(res.status).toBe(401);
   });
 
-  it(`if user access profile with correct jwt token, it should return 200`, async () => {
+  it(`Auth: if user access profile with correct jwt token, it should return 200`, async () => {
     return request(app.getHttpServer())
       .get('/profile')
       .set('Authorization', `Bearer ${jwtToken}`)
       .expect(200);
   });
 
-  it(`if email exists, then should return 200`, async () => {
+  it(`Auth: if email exists, then should return 200`, async () => {
     return request(app.getHttpServer())
       .post('/auth/search')
       .send({ email: 'rashikzaman22@gmail.com' })
       .expect(200);
   });
 
-  it(`if email does not exist, then should return 404`, async () => {
+  it(`Auth: if email does not exist, then should return 404`, async () => {
     return request(app.getHttpServer())
       .post('/auth/search')
       .send({ email: 'wer23444rashikzaman22@gmail.com' })
@@ -86,50 +86,50 @@ describe('App Api', () => {
 
   /** Resources Begin */
 
-  // it(`if requesting without proper authorization, it should return 401`, () => {
-  //   return request(app.getHttpServer()).post('/resources').expect(401);
-  // });
+  it(`if requesting without proper authorization, it should return 401`, () => {
+    return request(app.getHttpServer()).post('/user-resources').expect(401);
+  });
 
-  // it(`Resource: if requesting without required request body, it should return 400`, () => {
-  //   return request(app.getHttpServer())
-  //     .post('/resources')
-  //     .set('Authorization', `Bearer ${jwtToken}`)
-  //     .expect(400);
-  // });
+  it(`Resource: if requesting without required request body, it should return 400`, () => {
+    return request(app.getHttpServer())
+      .post('/user-resources')
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .expect(400);
+  });
 
-  // it(`Resource: if requesting with required request body, it should return 201`, async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .post('/resources')
-  //     .send({
-  //       title: 'test resource',
-  //       resourceTypeId: '1',
-  //       visibilityTypeId: '3',
-  //     })
-  //     .set('Authorization', `Bearer ${jwtToken}`);
-  //   expect(result.status).toBe(201);
-  //   resourceId = result.body.id;
-  // });
+  it(`Resource: if requesting with required request body, it should return 201`, async () => {
+    const result = await request(app.getHttpServer())
+      .post('/user-resources')
+      .send({
+        title: 'test resource',
+        resourceTypeId: '1',
+        visibilityTypeId: '3',
+      })
+      .set('Authorization', `Bearer ${jwtToken}`);
+    expect(result.status).toBe(201);
+    resourceId = result.body.id;
+  });
 
-  // it(`Resource: if requesting all resources, it should return 200`, async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .get('/resources')
-  //     .set('Authorization', `Bearer ${jwtToken}`);
-  //   expect(result.status).toBe(200);
-  // });
+  it(`Resource: if requesting all resources, it should return 200`, async () => {
+    const result = await request(app.getHttpServer())
+      .get('/user-resources')
+      .set('Authorization', `Bearer ${jwtToken}`);
+    expect(result.status).toBe(200);
+  });
 
-  // it(`Resource: if requesting one resources with parameter, it should return 200`, async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .get(`/resources/${resourceId}`)
-  //     .set('Authorization', `Bearer ${jwtToken}`);
-  //   expect(result.status).toBe(200);
-  // });
+  it(`Resource: if requesting one resources with parameter, it should return 200`, async () => {
+    const result = await request(app.getHttpServer())
+      .get(`/user-resources/${resourceId}`)
+      .set('Authorization', `Bearer ${jwtToken}`);
+    expect(result.status).toBe(200);
+  });
 
-  // it(`Resource: if deleting one resource with parameter, it should return 200`, async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .delete(`/resources/${resourceId}`)
-  //     .set('Authorization', `Bearer ${jwtToken}`);
-  //   expect(result.status).toBe(200);
-  // });
+  it(`Resource: if deleting one resource with parameter, it should return 200`, async () => {
+    const result = await request(app.getHttpServer())
+      .delete(`/user-resources/${resourceId}`)
+      .set('Authorization', `Bearer ${jwtToken}`);
+    expect(result.status).toBe(200);
+  });
 
   /** Resources End */
 

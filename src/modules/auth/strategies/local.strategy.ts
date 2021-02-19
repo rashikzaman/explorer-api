6081f58344a9411183539bb5ae801f78138c05cf
rashikIcarus <36659537+rashikIcarus@users.noreply.github.com>
2021-02-19 20:50:17 +0600

    Removed isVerified check of user from validation

--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -18,10 +18,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     if (!user) {
       throw new UnauthorizedException();
     }
-    if (!user.isVerified) {
-      //if user is not verified, throw unauthorized exception
-      throw new UnauthorizedException();
-    }
+
     return user;
   }
 }

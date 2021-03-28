e29dabab7c92a82db1d8d070d76deb48e5528180
styline <rashikzaman13@gmail.com>
2021-03-29 01:16:08 +0600

    Added 404 route not found error

--- src/modules/core/core.controller.ts
+++ src/modules/core/core.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException } from '@nestjs/common';
 import { CoreService } from './core.service';
 
-@Controller('core')
+@Controller()
 export class CoreController {
   constructor(private readonly coreService: CoreService) {}
+
+  @Get('*') //core module needs to be imported at last so that a notFoundException could be thrown if api is not found
+  returnNotFound(): string {
+    throw new NotFoundException('Route Not Found');
+  }
 }

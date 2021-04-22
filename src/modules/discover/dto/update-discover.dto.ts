f66a976e66a30bf0c8e71f3fc3956273df63a743
styline <rashikzaman13@gmail.com>
2021-04-23 00:29:14 +0600

    Added wonderId to update resource api

--- src/modules/discover/dto/update-discover.dto.ts
+++ src/modules/discover/dto/update-discover.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateDiscoverDto } from './create-discover.dto';
 
 export class UpdateDiscoverDto extends PartialType(CreateDiscoverDto) {}
--- src/modules/search/dto/update-search.dto.ts
+++ src/modules/search/dto/update-search.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateSearchDto } from './create-search.dto';
 
 export class UpdateSearchDto extends PartialType(CreateSearchDto) {}
